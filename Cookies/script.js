e9457f0b260e62af6bc59a22bb6439b88a266e7c
AryanSingh257 <aryan.singh.792166@gmail.com>
2026-09-18 19:52:53 +0530

    some minor fix ups

--- Cookies/script.js
+++ Cookies/script.js
@@ -1,33 +1,34 @@
-const card = document.querySelector("#cookie-card")
+const card = document.querySelector("#cookie-card");
 const Duration = 1000;
 
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve,ms));
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function cardPopUp() {
-    await sleep(Duration);
-    card.classList.add("show"); 
+  await sleep(Duration);
+  card.classList.add("show");
 }
 
 const Button = document.querySelectorAll("button");
 
-Button.forEach((button)=>{
-    button.addEventListener("click",()=>{
-        localStorage.setItem("visited",true);
-    });
+Button.forEach((button) => {
+  button.addEventListener("click", () => {
+    localStorage.setItem("visited", true);
+    card.classList.remove("show");
+  });
 });
 
 const header = document.querySelector("h1");
 
-for(let i=0;i<3;i++){
-    header.innerText = `${3-i}`;
-    await sleep(1000);
+for (let i = 0; i < 3; i++) {
+  header.innerText = `${3 - i}`;
+  await sleep(1000);
 }
 
-if (!localStorage.getItem("visited")){
-    header.innerText = "Please provide some cookies ^_^";
-    cardPopUp();
-}else{
-    header.innerText = "Thank you for visiting again";
+if (!localStorage.getItem("visited")) {
+  header.innerText = "Please provide some cookies ^_^";
+  cardPopUp();
+} else {
+  header.innerText = "Thank you for visiting again";
 }
